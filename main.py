b3889af2d616bf980328b3f65a0efc10cfeb1919
Akshay Ajbani <aajbani@bu.edu>
2015-11-25 21:07:12 -0500

    Housekeeping and minor file changes Moved data files into "Data" folder. Deleted test.txt and helloworld.py. Both files were useless tests to ensure that we could push files to the repository. main.py: added importData function that moved file opening to separate function and changed it so that it always pulls data from the "Data" folder without requiring the filename to include "./Data/filename"

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
+__author__ = 'Akshay Ajbani, Emily Misnick, Lucas Neves'
+
+
 import os
 import numpy as np
 import sys
 import random
 import math
-
+def importData(filename):
+    filename = "./Data/" + filename
+    with open(filename) as f:
+        ncols = len(f.readline().split('\t')) #gets number of columnts
+    data = np.loadtxt(filename, delimiter="\t",unpack=False,skiprows=1,usecols=range(4,ncols)) #skips text containing columns and rows to load only expression data
+    return data
 
 def main():
-	with open("G1_singlecells_counts.txt") as f:
-		ncols = len(f.readline().split('\t')) #gets number of columnts
-	data = np.loadtxt('G1_singlecells_counts.txt', delimiter="\t",unpack=False,skiprows=1,usecols=range(4,ncols)) #skips text containing columns and rows to load only expression data
-
+    data = importData("G1_singlecells_counts.txt")
 
 main()
