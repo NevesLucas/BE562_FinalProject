291b0805ab0090d7a105dae3a65d2cb7b439e980
Akshay Ajbani <aajbani@bu.edu>
2015-11-25 21:07:12 -0500

    Moved data files into "Data" folder.
    main.py:
    added importData function that moved file opening to separate function and changed it so that it always pulls data from the "Data" folder without requiring the filename to include "./Data/filename"

--- main.py
+++ main.py
@@ -3,12 +3,14 @@ import numpy as np
 import sys
 import random
 import math
-
+def importData(filename):
+    filename = "./Data/" + filename
+    with open(filename) as f:
+        ncols = len(f.readline().split('\t')) #gets number of columnts
+    data = np.loadtxt(filename, delimiter="\t",unpack=False,skiprows=1,usecols=range(4,ncols)) #skips text containing columns and rows to load only expression data
+    return data
 
 def main():
-	with open("G1_singlecells_counts.txt") as f:
-		ncols = len(f.readline().split('\t')) #gets number of columnts
-	data = np.loadtxt('G1_singlecells_counts.txt', delimiter="\t",unpack=False,skiprows=1,usecols=range(4,ncols)) #skips text containing columns and rows to load only expression data
-
+    data = importData("G1_singlecells_counts.txt")
 
 main()
