ebee520471a2e9b9fab275291200bd794e9ce6e6
NevesLucas <lneves@bu.edu>
2015-11-28 14:38:38 -0500

    added import for all classes, created SVM training function

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ __author__ = 'Akshay Ajbani, Emily Misnick, Lucas Neves'
 import os
 import numpy as np
 import sys
-import random
 import math
 import csv
+from sklearn.svm import SVC
 
 def importData(filename):
     with open(filename) as f:
@@ -21,12 +21,38 @@ def importProteinLabels(filename):
 		for rows in reader:
 			labels.append(rows[2])
 	return labels
-def TrainSVM(data,protein_labels,label):
+
+def TrainSVM(data,labels):
+	clf = SVC()
+	SVC(probability=true,random_state=22003)
+	clf.fit(data,Labels)
+	return clf
 
 
 def main():
-	filename = "./Data/G1_singlecells_counts.txt"
-    data = importData(filename)
-    protein_labels = importProteinLabels(filename)
+	filename1 = "./Data/G1_singlecells_counts.txt"
+	filename2 = "./Data/G2M_singlecells_counts.txt"
+	filename3 = "./Data/S_singlecells_counts.txt"
+	labels[]
+
+	protein_labels = importProteinLabels(filename1)
+	
+	data1 = importData(filename1)
+	for i in range(0,data1[1]):
+		labels.append(1)
+	data1=data1.transpose()
+
+	data2 = importData(filename2)
+	for i in range(0,data2[1]):
+		labels.append(2)
+	data2=data2.transpose()
+
+	data3 = importData(filename3)
+	for i in range(0,data3[1]):
+		labels.append(3)
+	data3=data3.transpose()
+
+
+
 
 main()
