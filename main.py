f04f18647bfd58fd2d3556d8d16b93814bf5533d
Akshay Ajbani <aajbani@bu.edu>
2015-11-28 21:58:40 -0500

    Training Vs Test Data
    Split the data so that it outputs into 2 different variables. Training data and test data.

--- main.py
+++ main.py
@@ -8,11 +8,18 @@ import math
 import csv
 from sklearn.svm import SVC
 
+num_rows = 38294
+
 def importData(filename):
-    with open(filename) as f:
-        ncols = len(f.readline().split('\t')) #gets number of columnts
-    data = np.loadtxt(filename, delimiter="\t",unpack=False,skiprows=1,usecols=range(4,ncols)) #skips text containing columns and rows to load only expression data
-    return data
+	with open(filename) as f:
+		ncols = len(f.readline().split('\t')) #gets number of columnts
+	train_data = np.loadtxt(filename, delimiter="\t",unpack=False,skiprows=1,usecols=range(4,ncols-16)) #skips text containing columns and rows to load only expression data
+	test_data = np.loadtxt(filename, delimiter="\t",unpack=False,skiprows=1,usecols=range(80,ncols)) #skips text containing columns and rows to load only expression data
+	train_data = train_data[:num_rows]
+	test_data = test_data[:num_rows]
+	return train_data, test_data
+
+
 def importProteinLabels(filename):
 	labels=[]
 	with open(filename,'r') as f:
@@ -24,8 +31,8 @@ def importProteinLabels(filename):
 
 def TrainSVM(data,labels):
 	clf = SVC()
-	SVC(probability=true,random_state=22003)
-	clf.fit(data,Labels)
+	SVC(probability= True,random_state=22003)
+	clf.fit(data,labels)
 	return clf
 
 
@@ -33,24 +40,41 @@ def main():
 	filename1 = "./Data/G1_singlecells_counts.txt"
 	filename2 = "./Data/G2M_singlecells_counts.txt"
 	filename3 = "./Data/S_singlecells_counts.txt"
-	labels[]
+	train_labels = []
+	test_labels = []
 
 	protein_labels = importProteinLabels(filename1)
-	
-	data1 = importData(filename1)
-	for i in range(0,data1[1]):
-		labels.append(1)
-	data1=data1.transpose()
 
-	data2 = importData(filename2)
-	for i in range(0,data2[1]):
-		labels.append(2)
-	data2=data2.transpose()
+	# Data 1
+	train_data1, test_data1 = importData(filename1)
+	for i in range(0,train_data1[1]):
+		train_labels.append(1)
+	for i in range(0,test_data1[1]):
+		test_labels.append(1)
+	train_data1 = train_data1.transpose()
+	test_data1 = test_data1.transpose()
 
-	data3 = importData(filename3)
-	for i in range(0,data3[1]):
-		labels.append(3)
-	data3=data3.transpose()
+	# Data 2
+	train_data2, test_data2 = importData(filename2)
+	for i in range(0,train_data2[1]):
+		train_labels.append(1)
+	for i in range(0,test_data2[1]):
+		test_labels.append(1)
+	train_data2 = train_data2.transpose()
+	test_data2 = test_data2.transpose()
+
+	# Data 3
+	train_data3, test_data3 = importData(filename3)
+	for i in range(0,train_data3[1]):
+		train_labels.append(1)
+	for i in range(0,test_data3[1]):
+		test_labels.append(1)
+	train_data3 = train_data3.transpose()
+	test_data3 = test_data3.transpose()
+
+
+
+
 
 
 
