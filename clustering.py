5dbf525405e06ce143e3c8dfd2b1462e24b72f7a
lneves <lneves@bu.edu>
2015-12-10 02:53:46 -0500

    all clustering data added

--- clustering.py
+++ clustering.py
@@ -30,24 +30,36 @@ def getInd(user_labels,labels):
         else:
             ind.append(val)
     return ind
+def importData(filename):
+    with open(filename) as f:
+        ncols = len(f.readline().split('\t')) #gets number of columns
+    data = np.loadtxt(filename, delimiter="\t",unpack=False,skiprows=1,usecols=range(4,ncols))
+    return data
 
 
 def main():
-    labels = importText('Data/sampleDataLabels.txt')
+    labels = importText('./Data/proteinlabels.txt')
+
     for i in range(len(labels)):
         labels[i]=labels[i].lower()
+    
     user_labels = importText('user_labels.txt')
     for i in range(len(user_labels)):
         user_labels[i]=user_labels[i].lower()
+
     ind = getInd(user_labels,labels)
-    test_data = np.genfromtxt('./Data/sampleData.csv')
+    
+    test_data = importData('./Data/G2M_singlecells_counts.txt')
+    
     test_data = test_data[ind,:]
+    
     print(ind)
 
     test_data=test_data.transpose()
-
+    print(test_data[1])
+    
     kmeans =  c.KMeans(n_clusters=5,random_state=180,n_jobs=1,init='k-means++').fit_predict(test_data)
-    np.savetxt('cluster_out.txt',kmeans,delimiter="\t")
+    np.savetxt('G2Mcluster_out.txt',kmeans,delimiter="\t")
     print(kmeans)
 
 
